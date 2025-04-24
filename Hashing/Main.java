eec438b981d449e78ce146c183937a4311580135
nagaraj6618 <nagaraj516700@gmail.com>
2025-04-24 17:46:10 +0530

    HashMap exercise

--- Hashing/Main.java
+++ Hashing/Main.java
@@ -1,11 +1,21 @@
+import java.util.*;
 
 public class Main{
    public static void main(String[]args){
 
 
-      FrequencyProgram obj = new FrequencyProgram();
+      // FrequencyProgram obj = new FrequencyProgram();
       // obj.frequencyOfNumberExample();
       // obj.frequencyofCharInStringOnlyLowerCaseAlphabets();
-      obj.frequencyofCharInString();
-   }
+      // obj.frequencyofCharInString();
+
+      //HashMap learning.
+      Map<String,Integer> hashObj = new HashMap<>();
+      hashObj.put("NagarajS",100);
+      hashObj.put("Arjun",20);
+      hashObj.put("deva",20);
+
+      System.out.println(hashObj.containsValue(20));
+      System.out.println(hashObj);
+   }  
 }
