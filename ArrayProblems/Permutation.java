96eda61d01303b2166be83d4e25be3b2e0d191d5
nagaraj6618 <nagaraj516700@gmail.com>
2025-05-18 21:28:22 +0530

    Next Permutation

--- ArrayProblems/Permutation.java
+++ ArrayProblems/Permutation.java
@@ -48,16 +48,59 @@ public class Permutation{
       }
       System.out.println();
    }
+
+
+   //Next Permutation
+   public int [] nextPermutation(int nums[]){
+      int n = nums.length;
+      int index = -1;
+
+      //Find the longest prefix nums[i]<nums[i+1]
+
+      for(int i=n-2;i>=0;i--){
+         if(nums[i]<nums[i+1]){
+            System.out.println("Index : "+i);
+            index = i;
+            break;
+         }
+      }
+      if(index == -1){
+         reverseArray(nums,0,nums.length-1);
+         printArray(nums);
+         return nums;
+      }
+      for(int i=n-1;i>index;i--){
+         if(nums[index]<nums[i]){
+            swap(nums, index, i);
+            break;
+         }
+      }
+      reverseArray(nums, index+1, nums.length-1);
+      printArray(nums);
+      return nums;
+
+   }
+   public void reverseArray(int []nums,int i,int j){
+      // int n = nums.length;
+      
+      while(i<j){
+         swap(nums, i, j);
+         i++;
+         j--;
+      }
+   }
    public static void main(String[] args){
       Permutation obj = new Permutation();
 
-      int [] nums = {1,2,3};
+      int [] nums = {2,1,5,4,3,0,0};
       List<List<Integer>> ans = new ArrayList<>();
       List<Integer> ds = new ArrayList<>();
       boolean[]freq = new boolean[nums.length];
 
       // obj.recurPermutation(nums, ans, ds, freq);
-      obj.recurPermutationMethod2(0, nums, ans);
-      System.out.println("All Permutation : "+ans);
+      // obj.recurPermutationMethod2(0, nums, ans);
+      // System.out.println("All Permutation : "+ans);
+
+      obj.nextPermutation(nums);
    }
 }
