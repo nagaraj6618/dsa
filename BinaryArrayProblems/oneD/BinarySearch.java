e3f1be4aa7422ce3d1a4f0cbf4a6d702f98f0d50
nagaraj6618 <nagaraj516700@gmail.com>
2025-06-15 23:58:41 +0530

    Updated

--- BinaryArrayProblems/oneD/BinarySearch.java
+++ BinaryArrayProblems/oneD/BinarySearch.java
@@ -50,4 +50,72 @@ public class BinarySearch {
       }
       return ans;
    }
+
+   //Floor and Ceil in sorted array;
+   public void floorAndCeil(int nums[],int target){
+      int n = nums.length;
+      int low = 0;
+      int high = n-1;
+      int floor = -1;
+      int ceil = -1; 
+      while(low<=high){
+         int mid = (low+high)/2;
+
+         if(nums[mid] == target){
+            floor = nums[mid];
+            ceil = nums[mid];
+            break;
+         }
+         else if(target>nums[mid]){
+            floor = nums[mid];
+            low = mid+1;
+         }else{
+            ceil = nums[mid];
+            high = mid-1;
+         }
+      }
+      System.out.println("Floor and ceil is : "+floor+" "+ceil);
+   }
+
+   //Find the occurance of the element
+   //Formula. = lastOccurance - firstOccurance+1 = Tot no of occurance.
+
+   public int totalOccuranceOfNumber(int nums[],int target){
+      int firstOccurance = -1;
+      int lastOccurance = -1;
+      int n = nums.length;
+      int low = 0;
+      int high = n-1;
+
+      while(low<=high){
+         int mid = (low+high)/2;
+
+         if(nums[mid] >= target){
+            if(nums[mid] == target){
+               firstOccurance = mid;
+            }
+            high = mid-1;
+         }else{
+            low = mid+1;
+         }
+      }
+      low = 0;
+      high = n-1;
+      while(low<=high){
+         int mid = (low+high)/2;
+         if(nums[mid]>target){
+            high = mid-1;
+         }else{
+            if(nums[mid] == target){
+               lastOccurance = mid;
+            }
+            low = mid+1;
+         }
+      }
+      int tot = 0;
+      if(firstOccurance != -1 && lastOccurance != -1){
+         tot = lastOccurance - firstOccurance+1;
+      }
+      return tot;
+   }
 }
--- BinaryArrayProblems/oneD/Main.java
+++ BinaryArrayProblems/oneD/Main.java
@@ -8,5 +8,16 @@ public class Main{
 
       int nums2[] = {3,5,8,9,15,19};
       System.out.println("The Lower Bound at : "+bobj.loweBound(nums2,9));
+
+      //Floor and Ceil
+      int nums3[] = {10,12,20,30,40,50};
+      int nums4[] = {3, 4, 4, 7, 8, 10};
+      bobj.floorAndCeil(nums3,12);
+      bobj.floorAndCeil(nums4,2);
+
+      //Find the total Occurance of the given target;
+      int nums5[] = {2, 2 , 3 , 3 , 3 , 3 , 4};
+      System.out.println("The Total occurance : "+bobj.totalOccuranceOfNumber(nums5,4));
+
    }
 }
